2786e99c94b7a7736195caced50d5a122280f5e8
DhruvaVetsa12ABC <95221871+DhruvaVetsa12ABC@users.noreply.github.com>
2022-06-23 21:19:56 +0530

    HW2 Commit

---
+++ HW2 - Using Data Types/program.java
@@ -0,0 +1,22 @@
+public class program {
+    public static void main(String[] args) {
+        byte b = 127;
+        int i = 231313483;
+        short s = 3831;
+        long l = 328307344;
+        String S = "My Name is Dhruva";
+        char c = 'D';
+        boolean B = true;
+        float f = 213123.129831289f;
+        double d = 39838917132391.19813987893271720137d;
+        System.out.println(b + " has a data type of byte.");
+        System.out.println(i + " has a data type of int.");
+        System.out.println(s + " has a data type of short.");
+        System.out.println(l + " has a data type of long.");
+        System.out.println("\"" + S  + "\" has a data type of String.");
+        System.out.println("\'" + c + "\' has a data type of char.");
+        System.out.println(B + " has a data type of boolean.");
+        System.out.println(f + " has a data type of float.");
+        System.out.println(d + " has a data type of double.");
+    }
+}
