6d4fd1359114c5de2965c37bbcf374e9644bce7a
DhruvaVetsa12ABC <95221871+DhruvaVetsa12ABC@users.noreply.github.com>
2023-01-23 20:05:08 +0530

    Commit

---
+++ JAVA WS/Term 2/Main II/test2.java
@@ -0,0 +1,9 @@
+public class test2 {
+
+  public static void main(String args[]) { // Was public static void main (String args[] {
+    int i = 10;
+    while (i <= 50) { // Was while(i <= 50); {
+      System.out.println(i); // Was system.out.println(i);
+    }
+  }
+}
